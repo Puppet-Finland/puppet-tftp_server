00e219253a02e63e34c05f17490d072a9be4253b
Samuli Seppänen <samuli.seppanen@gmail.com>
2017-07-07 15:24:24 +0300

    Remove CentOS 6 support, add Debian support
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,42 +1,12 @@
 #
 # == Class: tftp_server
 #
-class tftp_server
-(
-
-  $root_dir = '/var/lib/tftpboot',
-  $server_args = '-c -p -u tftpd -U 117 -s /var/lib/tftpboot -vvv',
-  $user = 'tftpd',
-  $group = 'tftpd',
-  
-) inherits tftp_server::params
+class tftp_server inherits tftp_server::params
 {
     
-  include os
-  include stdlib
-  include xinetd
-  
-  anchor { 'tftp_server::begin':
-    notify => Class['tftp_server::service'],
-  }
-  
-  class { 'tftp_server::install':
-    require => Anchor['tftp_server::begin'],
-    notify  => Class['tftp_server::config'],
-  }
-  
-  class { 'tftp_server::config':
-    server_args => $server_args,
-    user        => $user,
-    group       => $group,
-    require     => Class['tftp_server::install'],
-    notify      => Class['tftp_server::service'],
-  }
-  
-  class { 'tftp_server::service':
-    notify => Anchor['tftp_server::end'],
-  }
-  anchor {'tftp_server::end':
-  }
+  include ::os
+  include ::stdlib
+  include ::tftp_server::install
+  include ::tftp_server::service
 }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -6,13 +6,9 @@
 class tftp_server::params {
   
   case $::osfamily {
-    'RedHat': {
-      $package_name = [ 'tftp-server' ]
-      $service_name = 'xinetd'
-      $config_name = '/etc/xinetd.d/tftp'
-      $root_dir = '/var/lib/tftpboot'
-      $user = 'tftpd'
-      $group = 'tftpd'
+    'Debian': {
+      $package_name = 'tftpd-hpa'
+      $service_name = 'tftpd-hpa'
     }
     default: {
       fail("Unsupported OS: ${::osfamily}")
